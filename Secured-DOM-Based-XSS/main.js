dc431a86269fbc88eb09de3fe1f9b518dce4374c
Matthew Crutcher <matthew.crutcher@hotmail.com>
2026-02-02 10:40:28 +0800

    Added secured DOM based XSS code files

---
+++ Secured-DOM-Based-XSS/main.js
@@ -0,0 +1,48 @@
+var queryString = window.location.search;
+var searchQuery = new URLSearchParams(queryString).get('search');
+
+
+if (searchQuery) {
+    document.getElementById('search_string')
+    //simple mitigation as it is rendered as text not HTML
+        .textContent = 'Search results for "' + searchQuery + '"';
+    document.getElementById('search_text').value = searchQuery;
+    document.getElementById("search_results").classList.remove("hide");
+    document.getElementById("game_preview").classList.add("hide");
+}
+
+function profileAction(actionNum) {
+    switch (Number(actionNum)) {
+        case 0: linkSocialMedia(); break;
+        case 1: deleteAccount(); break;
+        case 2: changePassword(); break;
+        case 3: logout(); break;
+        default: alert("Error");
+    }
+}
+
+function logout() {
+    alert("Logging out...");
+}
+
+function deleteAccount() {
+    alert("Deleting Account...");
+}
+
+function changePassword() {
+    alert("Changing password...");
+}
+
+function linkSocialMedia() {
+    alert("Linking Social Media...");
+}
+
+function playGame() {
+    window.location = "/";
+}
+
+function sendCoins() {
+    var sendToUser = document.getElementById('send_to_user').value;
+    var numCoins = document.getElementById('num_coins').value;
+    alert("Sending " + numCoins + " coins to " + sendToUser);
+}
